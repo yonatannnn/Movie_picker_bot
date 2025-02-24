481bbd0cd9d75ba11f19949abf79cf624e027ff9
yonathan <yonatanalebachew7@gmail.com>
2025-02-24 08:13:34 +0300

    time changed

--- bot.py
+++ bot.py
@@ -187,7 +187,8 @@ async def delete_movie(event):
 async def schedule_movie_sending():
     while True:
         now = datetime.now()
-        if now.weekday() == 0 and now.hour == 8 and now.minute == 7:  # Monday at 8:00 AM
+        print(now.weekday(), now.hour, now.minute)
+        if now.weekday() == 0 and now.hour == 8 and now.minute == 15:  # Monday at 8:00 AM
             print(now.weekday(), now.hour, now.minute)
             groups = groups_collection.find()
             for group in groups:
